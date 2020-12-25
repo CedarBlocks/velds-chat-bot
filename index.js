182f74529aa30556c5ca4947fefdecc11cb1d987
Cinnamonsroll <71993095+Cinnamonsroll@users.noreply.github.com>
2020-12-25 16:41:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
 const socketClient = require("socket.io-client");
 const axios = require("axios");
-const ms = require('ms');
+const ms = require("ms");
 
 const connection = socketClient("https://chat-gateway.veld.dev/");
 console.log(`starting`);
 
 const members = {};
 const channels = {};
+let commands = [];
 let currentUser = null;
 let token = null;
 let uptime = 0;
-
-connection.on('connect', () => {
+connection.on("connect", () => {
   console.log("connected to gateway");
   connection.emit("login", {
     token: null,
     bot: true
-  })
+  });
   setInterval(() => {
-  uptime++
+    uptime++;
   }, 1);
 });
-
-connection.on('connect_error', (error) => {
+connection.on("connect_error", error => {
   console.log(error);
 });
 
-connection.on('ready', (info) => {
-  console.log(info);
+connection.on("ready", info => {
   token = info.token;
   currentUser = info.user;
   for (let m of info.members) {
     members[m.id] = m;
-  };
+  }
   for (let c of info.channels) {
     channels[c.id] = c;
   }
 
   connection.emit("channel:message", {
-    message: "/nick BOT-miki"
+    message: "/nick Bread-bot"
   });
 });
 
-connection.on("channel:join", (user) => {
+connection.on("channel:join", user => {
   if (!currentUser) {
     return;
   }
@@ -51,103 +49,93 @@ connection.on("channel:join", (user) => {
     return;
   }
 
-  connection.emit('usr-msg', {
+  connection.emit("usr-msg", {
     content: "welcome " + user.name + "!"
   });
   members[user.id] = user;
-})
+});
 
-connection.on("channel:leave", (user) => {
-  connection.emit('usr-msg', {
+connection.on("channel:leave", user => {
+  connection.emit("usr-msg", {
     content: "bye " + user.name + " :sob:"
   });
   delete members[user.id];
-})
+});
 
-let prefix = "."
-let startTime = 0
-let latency = 0
+let prefix = "pog ";
+let startTime = 0;
+let latency = 0;
 
 setInterval(() => {
-  startTime = Date.now()
-  axios.get('https://chat.veld.dev').then(res => {
+  startTime = Date.now();
+  axios.get("https://chat.veld.dev").then(res => {
     latency = Date.now() - startTime;
-  })
+  });
 }, 5000);
+const fs = require("fs");
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.log(err);
 
-connection.on('message:create', async (message) => {
-  console.log(message);
-  if (!message.content.startsWith(prefix) || !message.content || members[message.user].bot) {
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if (jsfile.length <= 0) {
+    console.log("Could not find any commands");
     return;
   }
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
+  jsfile.forEach(f => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    commands[props.help.name] = props;
+  });
+});
 
-  if (command === 'ping') {
-    msgSend(`Pong!\n${latency}ms`);
-  } else if (command === 'avatar' || command === 'av') {
-    let member = message.mentions[0] ? members[message.mentions[0]] : members[message.user];
-    msgSend(`Here is ${member.name}'s avatar\n${member.avatarUrl}`);
-  } else if (command === 'say') {
-    msgSend(args.join(" "));
-  } else if (command === 'eval') {
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string") {
-        evaled = require("util").inspect(evaled);
-        msgSend(clean(evaled));
-      }
-    } catch (err) {
-      msgSend(`\`ERROR\` \`\n${clean(err)}\n\``);
-    };
-  } else if (command === 'userinfo' || command === 'ui') {
-    let user = message.mentions[0] ? members[message.mentions[0]] : members[message.user];
-    try {
-      await axios.post(`https://chat-gateway.veld.dev/api/v1/channels/${message.channelId}/messages`, {
-        embed: {
-          title: `Heres some info about ${user.name}`,
-          description: `**ID:** ${user.id}\n**Status:** ${user.status.value}`,
-          thumbnailUrl: user.avatarUrl,
-          footer: `Bot: ${user.bot ? user.bot + " ✅" : user.bot + " ❌"}`,
-          author: {
-            iconUrl: members[message.user].avatarUrl,
-            value: members[message.user].name
-          },
-        },
-      }, {
-        headers: {
-          Authorization: "Bearer " + token
-        }
-      })
-    } catch (err) {
-      console.log(err.response.data.details);
-      return;
-    }
-
+connection.on("message:create", async message => {
+  message.ping = latency
+  message.cats = ["general", "text"];
+  message.commands = commands;
+  if (
+    !message.content.startsWith(prefix) ||
+    !message.content ||
+    members[message.user].bot
+  ) {
+    return;
   }
-
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/);
+  let command = args.shift().toLowerCase();
+  let command2 = undefined;
+  if (commands[command]) {
+    command2 = commands[command];
+  }
+  if (command2 !== undefined) {
+    command2.run(message, token, members, args);
+  }
   function msgSend(msg) {
     (async () => {
-      await axios.post(`https://chat-gateway.veld.dev/api/v1/channels/${message.channelId}/messages`, {
-        content: msg,
-      }, {
-        headers: {
-          Authorization: "Bearer " + token
+      await axios.post(
+        `https://chat-gateway.veld.dev/api/v1/channels/${message.channelId}/messages`,
+        {
+          content: msg
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + token
+          }
         }
-      });
+      );
     })();
-  };
+  }
 
   function msgReply(msg) {
-    msgSend(`@${members[message.user].name} ${msg}`)
-  };
+    msgSend(`@${members[message.user].name} ${msg}`);
+  }
 
   function clean(text) {
-    if (typeof (text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-      return text;
+    if (typeof text === "string")
+      return text
+        .replace(/`/g, "`" + String.fromCharCode(8203))
+        .replace(/@/g, "@" + String.fromCharCode(8203));
+    else return text;
   }
 });
